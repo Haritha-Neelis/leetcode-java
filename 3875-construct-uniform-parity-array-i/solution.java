c08de244eff502a1fbe488bd4132b1f4d9ec0aae
Haritha N <haritha.neelisetty@gmail.com>
2026-09-02 13:29:26 +0530

    Time: 0 ms | Memory: 45.4 MB - LeetPush

--- 3875-construct-uniform-parity-array-i/solution.java
+++ 3875-construct-uniform-parity-array-i/solution.java
@@ -1,7 +1,7 @@
 // Construct Uniform Parity Array I
 // Difficulty: Easy
 // Runtime: 0 ms
-// Memory: 45 MB
+// Memory: 45.4 MB
 // https://leetcode.com/problems/construct-uniform-parity-array-i/
 
 class Solution {

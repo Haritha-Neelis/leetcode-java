f8e333ab85802538b4e6aeab27c324f6a8251394
Haritha N <haritha.neelisetty@gmail.com>
2026-09-02 13:25:48 +0530

    Time: 0 ms | Memory: 45 MB - LeetPush

---
+++ 3875-construct-uniform-parity-array-i/solution.java
@@ -0,0 +1,11 @@
+// Construct Uniform Parity Array I
+// Difficulty: Easy
+// Runtime: 0 ms
+// Memory: 45 MB
+// https://leetcode.com/problems/construct-uniform-parity-array-i/
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}

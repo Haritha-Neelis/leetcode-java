3e09cf59eca0005b8f3d9e956eb736e645593fca
Haritha N <haritha.neelisetty@gmail.com>
2026-09-03 07:27:39 +0530

    Time: 3 ms | Memory: 121.5 MB - LeetPush

---
+++ 3876-construct-uniform-parity-array-ii/solution.java
@@ -0,0 +1,25 @@
+// Construct Uniform Parity Array II
+// Difficulty: Medium
+// Runtime: 3 ms
+// Memory: 121.5 MB
+// https://leetcode.com/problems/construct-uniform-parity-array-ii/
+
+        }
+
+        if (minOdd == Integer.MAX_VALUE) {
+            return true;
+        // All numbers are even
+
+            }
+        }
+                minOdd = Math.min(minOdd, x);
+            } else {
+                minEven = Math.min(minEven, x);
+        for (int x : nums1) {
+            if ((x & 1) == 1) {
+        int minEven = Integer.MAX_VALUE;
+
+
+        int minOdd = Integer.MAX_VALUE;
+    public boolean uniformArray(int[] nums1) {
+class Solution {

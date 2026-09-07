94d0a0a11098a2fdc1c378f729b789fabbf55fbf
Haritha N <haritha.neelisetty@gmail.com>
2026-09-07 19:53:29 +0530

    Time: 3 ms | Memory: 71.3 MB - LeetPush

--- count-good-cyclic-rotations/solution.java
+++ count-good-cyclic-rotations/solution.java
@@ -1,7 +1,7 @@
 // Q2. Count Good Cyclic Rotations
 // Difficulty: Medium
 // Runtime: 3 ms
-// Memory: 71.6 MB
+// Memory: 71.3 MB
 // https://leetcode.com/problems/count-good-cyclic-rotations/
 
 # code not captured

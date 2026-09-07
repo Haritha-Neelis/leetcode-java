26d400dc794414e0fa30df6fbaedcf30d8e2edb9
Haritha N <haritha.neelisetty@gmail.com>
2026-09-07 13:24:11 +0530

    Time: 1 ms | Memory: 43.9 MB - LeetPush

--- count-rotations-with-exactly-k-equal-adjacent-pairs/solution.java
+++ count-rotations-with-exactly-k-equal-adjacent-pairs/solution.java
@@ -1,7 +1,7 @@
 // Q1. Count Rotations With Exactly K Equal Adjacent Pairs
 // Difficulty: Easy
 // Runtime: 1 ms
-// Memory: 43.7 MB
+// Memory: 43.9 MB
 // https://leetcode.com/problems/count-rotations-with-exactly-k-equal-adjacent-pairs/
 
 # code not captured

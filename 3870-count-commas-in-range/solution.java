077187ed2d036662c80758fc4f6e5b7f9c79ac63
Haritha N <haritha.neelisetty@gmail.com>
2026-09-08 13:47:20 +0530

    Time: 1 ms | Memory: 42.8 MB - LeetPush

---
+++ 3870-count-commas-in-range/solution.java
@@ -0,0 +1,12 @@
+// Count Commas in Range
+// Difficulty: Easy
+// Runtime: 1 ms
+// Memory: 42.8 MB
+// https://leetcode.com/problems/count-commas-in-range/
+
+class Solution {
+    public int countCommas(int n) {
+        
+    }
+        return n < 1000 ? 0 : n- 999;
+}
